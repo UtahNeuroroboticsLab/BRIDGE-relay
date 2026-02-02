0f7664be2cb2cfc737d22d20ac9e4cbbca6e9387
Clare Keeler <clke2905@colorado.edu>
2026-02-02 10:17:42 -0700

    Add files via upload

---
+++ rpi_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import socket
+
+print("Starting...\n")
+
+# Connect to the server (not bind!)
+client = socket.socket()
+client.connect(('127.0.0.1', 7632))
+print(f"Connected to server!\n")
+
+while True:
+    msg = client.recv(1024).decode().strip()
+    if not msg or msg == "bye":
+        break
+    
+    print(f"Received: {msg}")
+    
+    if msg == "1":
+        response = "Hello!"
+    else:
+        response = "Incorrect input, please type in a 1."
+    
+    print(f"Sending: {response}\n")
+    client.send(response.encode())
+
+client.close()
